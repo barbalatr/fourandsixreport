42f53db4f6535d0f2e75a729b73d63f2f57df5d8
Rodrigo Barbalat <barbalatr@gmail.com>
2019-05-07 19:28:23 -0300

    create database

--- fourandsix/src/Server.js
+++ fourandsix/src/Server.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
+const { Client } = require("pg");
 var PORT = process.env.PORT || 5000;
 const rootPath = url => path.join(process.cwd(), url);
+
+const client = new Client({
+  host: "localhost",
+  user: "barbalatr",
+  port: 5432,
+  database: "sptcdb"
+});
 
 const app = express()
   .use(bodyParser())
@@ -13,6 +21,18 @@ const app = express()
     response.sendFile(rootPath("./tmp/Browser.js"));
   })
   .post("/", (request, response) => {
+    client
+      .connect()
+      .then(() => console.log("Connected Successfully to Database"))
+      .then(() =>
+        client.query("insert into atendimento values ($1, $2, $3)", [
+          1,
+          34,
+          request.body.delegado
+        ])
+      )
+      .catch(e => console.log(e))
+      .finally(() => client.end());
     console.log(request.body);
     response.json({ message: "ok" });
   })
